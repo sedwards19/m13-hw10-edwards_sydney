9fe826319717f6dc471d0e103fa1fbea7686c043
Sydney Edwards <sydneyedwards@Sydneys-Air.lan>
2018-11-18 12:50:46 -0500

    Add JavaScript functionality

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 //JavaScript Functionalit Part 1 Goes Here. Comment it all out once finsihed.
+var section = document.getElementsByTagName('section');
 
+var button = document.getElementsByTagName('button');
+button.addEventListener('click', slideToggle);
+
+function slideToggle() {
+	var section = document.getElementsByTagName('section');
+	section.classList.toggle('hide');
+}
 
 
 
 //jQuery functionality will go here. Make sure to iclude the document.ready function.
+//$(document).ready(function(){
+	//$('button').click(function() {
+	//	$('div').slideToggle();
+	//});
+//});
