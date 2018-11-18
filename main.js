dd1b0e1c4b42842c6cdd456bba14ea2fb300eed9
Sydney Edwards <sydneyedwards@Sydneys-Air.lan>
2018-11-18 12:55:22 -0500

    Add jQuery to recreate slide toggle

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
 //JavaScript Functionalit Part 1 Goes Here. Comment it all out once finsihed.
-var section = document.getElementsByTagName('section');
+//var section = document.getElementsByTagName('section');
 
-var button = document.getElementsByTagName('button');
-button.addEventListener('click', slideToggle);
+//var button = document.getElementsByTagName('button');
+//button.addEventListener('click', slideToggle);
 
-function slideToggle() {
-	var section = document.getElementsByTagName('section');
-	section.classList.toggle('hide');
-}
+//function slideToggle() {
+	//var section = document.getElementsByTagName('section');
+	//section.classList.toggle('hide');
+//}
 
 
 
 //jQuery functionality will go here. Make sure to iclude the document.ready function.
-//$(document).ready(function(){
-	//$('button').click(function() {
-	//	$('div').slideToggle();
-	//});
-//});
+$(document).ready(function(){
+	$('button').click(function() {
+		$('div').slideToggle();
+	});
+});
